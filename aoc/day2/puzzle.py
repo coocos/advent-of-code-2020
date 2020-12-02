ca2435129f162413da5be1a77bbd13e001e5a56f
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-02 17:33:19 +0200

    Solve day 2 puzzles

---
+++ aoc/day2/puzzle.py
@@ -0,0 +1,54 @@
+from typing import List, Tuple, Counter
+from collections import namedtuple, Counter
+from pathlib import Path
+
+
+Policy = namedtuple("NamedTuple", ["low", "high", "letter"])
+
+
+def parse_input() -> List[Tuple[Policy, str]]:
+
+    with open(Path(__file__).parent / "input.txt") as f:
+        lines = [line.strip() for line in f.readlines()]
+
+    passwords = []
+    for line in lines:
+        policy, password = line.split(": ")
+        passwords.append((parse_policy(policy), password))
+    return passwords
+
+
+def parse_policy(policy: str) -> Policy:
+    limits, letter = policy.split(" ")
+    low, high = map(int, limits.split("-"))
+    return Policy(low, high, letter)
+
+
+def first_policy(policy: Policy, password: str) -> int:
+    letters = Counter(password)
+    return (
+        policy.letter in letters and policy.low <= letters[policy.letter] <= policy.high
+    )
+
+
+def second_policy(policy: Policy, password: str) -> bool:
+    first = password[policy.low - 1] == policy.letter
+    second = password[policy.high - 1] == policy.letter
+    return first != second
+
+
+if __name__ == "__main__":
+
+    passwords = parse_input()
+
+    # First part
+    assert (
+        sum(1 for policy, password in passwords if first_policy(policy, password))
+        == 378
+    )
+
+    # Second part
+    assert (
+        sum(1 for policy, password in passwords if second_policy(policy, password))
+        == 280
+    )

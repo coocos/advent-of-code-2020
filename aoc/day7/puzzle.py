e7cdd54715ec44f1ce452daaacf564dd55e02e62
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-07 18:09:55 +0200

    Clean up day 7 parsing

--- aoc/day7/puzzle.py
+++ aoc/day7/puzzle.py
@@ -1,3 +1,4 @@
+import re
 import collections
 from typing import Dict, Tuple, List
 from pathlib import Path
@@ -9,54 +10,50 @@ BagGraph = Dict[str, List[Tuple[str, int]]]
 def parse_input() -> BagGraph:
 
     with open(Path(__file__).parent / "input.txt") as f:
-        raw_rules = [line.strip().replace(",", "") for line in f]
+        rules = [line.strip().replace(",", "") for line in f]
+
+    pattern = r"(\w+ \w+) bags contain (.+)\."
+    inner_pattern = r"(\d+) (\w+ \w+) bags?"
 
     graph: BagGraph = {}
-    for raw_rule in raw_rules:
-        tokens = raw_rule.split(" ")
-        bag = " ".join(tokens[:2])
+    for rule in rules:
+        bag, inner_bags = re.match(pattern, rule).groups()
         graph[bag] = []
-        if tokens[4] == "no":
-            continue
-        nested_bags = tokens[4:]
-        while nested_bags:
-            graph[bag].append(
-                (f"{nested_bags[1]} {nested_bags[2]}", int(nested_bags[0]))
-            )
-            nested_bags = nested_bags[4:]
+        for quantity, name in re.findall(inner_pattern, inner_bags):
+            graph[bag].append((name, int(quantity)))
+
     return graph
 
 
-def contains_gold_bag(graph: BagGraph, bag: str) -> bool:
+def contains_bag(graph: BagGraph, bag: str, target: str) -> bool:
 
     for inner_bag, _ in graph[bag]:
-        if inner_bag == "shiny gold" or contains_gold_bag(graph, inner_bag):
+        if inner_bag == target or contains_bag(graph, inner_bag, target):
             return True
-
     return False
 
 
-def count_bags_inside_gold_bag(graph: BagGraph) -> int:
+def count_bags_within_bag(graph: BagGraph, bag: str) -> int:
 
-    total = 0
-    queue = collections.deque([("shiny gold", 1)])
+    bags = 0
+    queue = collections.deque([(bag, 1)])
 
     while queue:
         bag, quantity = queue.popleft()
-        total += quantity
+        bags += quantity
         for inner_bag, inner_quantity in graph[bag]:
             queue.append((inner_bag, quantity * inner_quantity))
 
     # Minus one because we don't want to count the shiny gold bag itself
-    return total - 1
+    return bags - 1
 
 
 if __name__ == "__main__":
 
-    rules = parse_input()
+    graph = parse_input()
 
     # First part
-    assert sum(1 for bag in rules if contains_gold_bag(rules, bag)) == 265
+    assert sum(1 for bag in graph if contains_bag(graph, bag, "shiny gold")) == 265
 
     # Second part
-    assert count_bags_inside_gold_bag(rules) == 14177
+    assert count_bags_within_bag(graph, "shiny gold") == 14177

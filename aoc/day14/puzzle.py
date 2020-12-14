2fe3748fe105b90a42f7ee12db883182c8a07f9f
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-14 18:10:58 +0200

    Clean up day 14 slightly

--- aoc/day14/puzzle.py
+++ aoc/day14/puzzle.py
@@ -1,63 +1,57 @@
 import re
-from typing import List
+from typing import List, Iterable
 from pathlib import Path
 
 
-def parse_input() -> List[str]:
+def parse_input() -> List[List[str]]:
     with open(Path(__file__).parent / "input.txt") as f:
-        return [line.strip() for line in f.readlines()]
+        return [line.strip().split(" = ") for line in f.readlines()]
 
 
-def execute_v1(instructions: List[str]):
+def execute_decoder_v1(instructions: List[List[str]]):
 
     memory = {}
 
-    for instruction in instructions:
-        operation, value = instruction.split(" = ")
+    for operation, value in instructions:
         if operation == "mask":
             ones = int(value.replace("X", "0"), base=2)
             zeroes = int(value.replace("X", "1"), base=2)
         else:
-            address = int(re.match(r"mem\[(\d+)\]", operation).group(1))
+            address = int(operation[4 : operation.index("]")])
             memory[address] = (int(value) | ones) & zeroes
 
     return sum(memory.values())
 
 
-def generate_masks(pre, post=""):
+def addresses(unapplied: List[str], applied: List[str] = None) -> Iterable[int]:
 
-    if not pre:
-        yield post
+    if not unapplied and applied:
+        yield int("".join(applied), base=2)
         return
 
-    if pre[0] == "X":
-        yield from generate_masks(pre[1:], post + "1")
-        yield from generate_masks(pre[1:], post + "0")
+    if applied is None:
+        applied = []
+
+    bit, *rest = unapplied
+    if bit == "X":
+        yield from addresses(rest, applied + ["1"])
+        yield from addresses(rest, applied + ["0"])
     else:
-        yield from generate_masks(pre[1:], post + pre[0])
+        yield from addresses(rest, applied + [bit])
 
 
-def execute_v2(instructions: List[str]):
+def execute_decoder_v2(instructions: List[List[str]]):
 
     memory = {}
 
-    for instruction in instructions:
-        operation, value = instruction.split(" = ")
+    for operation, value in instructions:
         if operation == "mask":
-            mask = value
+            bitmask = value
         else:
-            original_address = int(re.match(r"mem\[(\d+)\]", operation).group(1))
-            base_mask = []
-            for m, a in zip(mask, "{:036b}".format(original_address)):
-                if m == "1":
-                    base_mask.append("1")
-                elif m == "0":
-                    base_mask.append(a)
-                else:
-                    base_mask.append("X")
-            base_mask = "".join(base_mask)
-            for address in generate_masks(base_mask):
-                memory[int(address, base=2)] = int(value)
+            address = "{:036b}".format(int(operation[4 : operation.index("]")]))
+            mask = [a if m == "0" else m for m, a in zip(bitmask, address)]
+            for floating_address in addresses(mask):
+                memory[floating_address] = int(value)
 
     return sum(memory.values())
 
@@ -67,7 +61,7 @@ if __name__ == "__main__":
     instructions = parse_input()
 
     # First part
-    assert execute_v1(instructions) == 16003257187056
+    assert execute_decoder_v1(instructions) == 16003257187056
 
     # Second part
-    assert execute_v2(instructions) == 3219837697833
+    assert execute_decoder_v2(instructions) == 3219837697833

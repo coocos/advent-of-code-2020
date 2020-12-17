e9cd0098b7c4e30053e1f1f7981a0f454a035e4b
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-17 18:19:46 +0200

    Solve day 17 part 2

--- aoc/day17/puzzle.py
+++ aoc/day17/puzzle.py
@@ -1,53 +1,37 @@
+from typing import Dict, Tuple, Iterable
 from pathlib import Path
 
 
-def parse_input() -> str:
+def parse_input() -> Dict[Tuple, str]:
     with open(Path(__file__).parent / "input.txt") as f:
         lines = [line.strip() for line in f.readlines()]
 
-    cubes = {}
+    hypercubes: Dict[Tuple, str] = {}
     for y in range(len(lines)):
         for x in range(len(lines[0])):
-            cubes[(x, y, 0)] = lines[y][x]
-    return cubes
+            hypercubes[(x, y, 0, 0)] = lines[y][x]
+    return hypercubes
 
 
-def bounds(cubes):
+def neighbours(cube: Tuple) -> Iterable[Tuple]:
 
-    x = (min(cube[0] for cube in cubes.keys()), max(cube[0] for cube in cubes.keys()))
-    y = (min(cube[1] for cube in cubes.keys()), max(cube[1] for cube in cubes.keys()))
-    z = (min(cube[2] for cube in cubes.keys()), max(cube[2] for cube in cubes.keys()))
-    return (x, y, z)
-
-
-def neighbours(cube):
-
-    x, y, z = cube
     for dx in (-1, 1, 0):
         for dy in (-1, 1, 0):
             for dz in (-1, 1, 0):
-                neighbour = (x + dx, y + dy, z + dz)
-                if neighbour != cube:
-                    yield neighbour
-
-
-def draw(cubes):
-
-    x_range, y_range, z_range = bounds(cubes)
-    for z in range(z_range[0], z_range[1] + 1):
-        print(f"z={z}")
-        for y in range(y_range[0], y_range[1] + 1):
-            row = []
-            for x in range(x_range[0], x_range[1] + 1):
-                if (x, y, z) in cubes:
-                    row.append(cubes[(x, y, z)])
+                if len(cube) == 3:
+                    x, y, z = cube
+                    neighbour = (x + dx, y + dy, z + dz)
+                    if neighbour != cube:
+                        yield neighbour
                 else:
-                    row.append(".")
-            print("".join(row))
-    print("")
+                    x, y, z, w = cube
+                    for dw in (-1, 1, 0):
+                        neighbour = (x + dx, y + dy, z + dz, w + dw)
+                        if neighbour != cube:
+                            yield neighbour
 
 
-def step(cubes):
+def step(cubes: Dict[Tuple, str]) -> Dict[Tuple, str]:
 
     expanded = cubes.copy()
 
@@ -64,20 +48,17 @@ def step(cubes):
                 active_neighbours += 1
         if state == "#":
             if active_neighbours in (2, 3):
-                # print(f"Keeping {cube} as active")
                 expanded_copy[cube] = "#"
             else:
-                # print(f"Deactivating {cube}")
                 expanded_copy[cube] = "."
         else:
             if active_neighbours == 3:
-                # print(f"Activating {cube}")
                 expanded_copy[cube] = "#"
 
     return expanded_copy
 
 
-def active_cubes(cubes):
+def active_cubes(cubes: Dict[Tuple, str]) -> int:
 
     return len([cube for cube in cubes.values() if cube == "#"])
 
@@ -86,11 +67,20 @@ if __name__ == "__main__":
 
     first_gen = parse_input()
 
+    simpler_first_gen = {cube[:3]: value for cube, value in first_gen.items()}
     # First part
+    cycle = 1
+    next_gen = simpler_first_gen
+    while cycle <= 6:
+        next_gen = step(next_gen)
+        cycle += 1
+    assert active_cubes(next_gen) == 237
+
+
+    # Second part
     cycle = 1
     next_gen = first_gen
     while cycle <= 6:
         next_gen = step(next_gen)
         cycle += 1
-
-    assert active_cubes(next_gen) == 237
+    assert active_cubes(next_gen) == 2448

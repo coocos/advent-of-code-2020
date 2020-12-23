60065603582e5d38a2c0b949e3893bab4e5c5ef7
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-23 18:38:45 +0200

    Solve day 23 second part

--- aoc/day23/puzzle.py
+++ aoc/day23/puzzle.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
-from dataclasses import dataclass, field
-from collections import deque
 from pathlib import Path
-from typing import Deque, Dict, List, Optional
+from typing import Dict, List, Optional
 
 
 def parse_input() -> List[int]:
@@ -28,9 +26,6 @@ class Cup:
         self.next = cup
         cup.next = temp
 
-    def __repr__(self) -> str:
-        return str(self.label) + " -> " + str(self.next.label)
-
 
 def create_circular_list(labels: List[int]) -> Cup:
     head = Cup(labels[0])
@@ -53,36 +48,7 @@ def create_label_map(cup: Cup) -> Dict[int, Cup]:
     return labels
 
 
-def debug(cup: Cup):
-    labels = []
-    seen = set()
-    current = cup
-    while current:
-        if current.label in seen:
-            break
-        labels.append(current.label)
-        seen.add(current.label)
-        current = current.next
-    print(" ".join(map(str, labels)))
-
-
-def cups_as_str(cup: Cup, target: Cup) -> str:
-
-    labels = []
-    seen = set()
-    current = cup
-    while current:
-        if current.label in seen:
-            break
-        labels.append(
-            current.label if current.label != target.label else f"({current.label})"
-        )
-        seen.add(current.label)
-        current = current.next
-    return " ".join(map(str, labels))
-
-
-def play(cup_list: List[int], moves: int = 0) -> str:
+def play(cup_list: List[int], moves: int = 0) -> List[int]:
 
     cup = create_circular_list(cup_list)
     cups = create_label_map(cup)
@@ -123,3 +89,8 @@ if __name__ == "__main__":
 
     # First part
     assert "".join(map(str, play(cups, 100))) == "59374826"
+
+    # Second part
+    more_cups = cups + [cup for cup in range(10, 1_000_000 + 1)]
+    results = play(more_cups, 10_000_000)
+    assert results[0] * results[1] == 66878091588

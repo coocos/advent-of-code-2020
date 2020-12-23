4233a373146d9304b86edf7ad7d5139e20c949b9
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-23 13:33:51 +0200

    Remove unused template

--- aoc/day22/template/puzzle.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-def parse_input() -> str:
-    with open(Path(__file__).parent / "input.txt") as f:
-        return f.read()
-
-
-if __name__ == "__main__":
-    print(parse_input())

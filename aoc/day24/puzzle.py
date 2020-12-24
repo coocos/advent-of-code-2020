71a7bb20e81a34ca871799e07fbb6f81c4b309b2
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-12-24 09:44:09 +0200

    Solve day 24 second part

--- aoc/day24/puzzle.py
+++ aoc/day24/puzzle.py
@@ -1,5 +1,10 @@
-from typing import List, Iterable
+from collections import defaultdict
+from typing import List, Iterable, Dict, Tuple
 from pathlib import Path
+
+
+Coord = Tuple[int, int]
+Grid = Dict[Coord, str]
 
 
 def parse_input() -> List[str]:
@@ -7,18 +12,7 @@ def parse_input() -> List[str]:
         return [line.strip() for line in f]
 
 
-def next_step(instruction: str) -> Iterable[str]:
-
-    direction = ""
-    for char in instruction:
-        if direction in ["e", "se", "sw", "w", "nw", "ne"]:
-            yield direction
-            direction = ""
-        direction += char
-    yield direction
-
-
-def flip(instructions: List[str]):
+def next_step(instruction: str) -> Iterable[Coord]:
 
     directions = {
         "e": (1, 0),
@@ -28,17 +22,67 @@ def flip(instructions: List[str]):
         "nw": (0, -1),
         "ne": (1, -1),
     }
-    tiles = {}
+    direction = ""
+    for char in instruction:
+        if direction in directions:
+            yield directions[direction]
+            direction = ""
+        direction += char
+    yield directions[direction]
+
+
+def flip(instructions: List[str]) -> Grid:
+
+    tiles: Grid = {}
 
     for instruction in instructions:
-
         position = (0, 0)
-        for step in next_step(instruction):
-            x, y = directions[step]
+        for x, y in next_step(instruction):
             position = (position[0] + x, position[1] + y)
         tiles[position] = "black" if position not in tiles else "white"
 
     return tiles
+
+
+def neighbours(x: int, y: int) -> Iterable[Coord]:
+
+    for dx, dy in [(1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1)]:
+        yield x + dx, y + dy
+
+
+def automata(tiles: Grid) -> Grid:
+
+    expanded = tiles.copy()
+    for position in tiles:
+        for neighbour in neighbours(*position):
+            if neighbour not in tiles:
+                expanded[neighbour] = "white"
+
+    next_gen = tiles.copy()
+
+    for position, tile in expanded.items():
+
+        black_tiles = 0
+        for neighbour in neighbours(*position):
+            if expanded.get(neighbour, "white") == "black":
+                black_tiles += 1
+
+        if tile == "white" and black_tiles == 2:
+            next_gen[position] = "black"
+        elif tile == "black" and (black_tiles == 0 or black_tiles > 2):
+            next_gen[position] = "white"
+
+    return next_gen
+
+
+def simulate(tiles: Grid, days: int = 100) -> Grid:
+
+    current_gen = tiles
+    while days > 0:
+        current_gen = automata(current_gen)
+        black_tiles = len([color for color in current_gen.values() if color == "black"])
+        days -= 1
+    return current_gen
 
 
 if __name__ == "__main__":
@@ -48,3 +92,7 @@ if __name__ == "__main__":
     # First part
     tiles = flip(instructions)
     assert len([color for color in tiles.values() if color == "black"]) == 293
+
+    # Second part
+    tiles = simulate(tiles)
+    assert len([color for color in tiles.values() if color == "black"]) == 3967
